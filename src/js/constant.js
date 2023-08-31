35a2e80ddb28d05b10e47ff1d72592d3bf0b0e13
Margarita <Margarita>
2023-08-31 16:42:03 +0300

    feat: add constants

---
+++ src/js/constant.js
@@ -0,0 +1,5 @@
+export const  IMAGE_WIDTH = 1920,
+              IMAGE_HEIGHT = 1080,
+							IMAGE_CENTER_X = IMAGE_WIDTH / 2,
+							IMAGE_CENTER_Y = IMAGE_HEIGHT / 2,
+							BUTTON_HEIGHT = 41;
