e6a7da20d5dcdab233fc28999648ff984bba3228
Margarita Sipach <99031179+Margarita-Sipach@users.noreply.github.com>
2023-08-31 22:15:52 +0300

    Revert "Revert "Develop""

---
+++ src/js/data.js
@@ -0,0 +1,56 @@
+export const buttonsData = [
+	{
+		color: "green",
+		text: "Стадион",
+		imageX: 687,
+		imageY: 482
+	},
+	{
+		color: "green",
+		text: "Зеленый дом",
+		imageX: 1148,
+		imageY: 404
+	},
+	{
+		color: "green",
+		text: "Дом около моря",
+		imageX: 1107,
+		imageY: 790
+	},
+	{
+		color: "green",
+		text: "Дом около стадиона",
+		imageX: 1306,
+		imageY: 669
+	},
+	{
+		text: "Мост",
+		imageX: 446,
+		imageY: 714
+	},
+	{
+		text: "Стадион 2 корпус",
+		imageX: 1081,
+		imageY: 629
+	},
+	{
+		text: "Парковка",
+		imageX: 580,
+		imageY: 282
+	},
+	{
+		text: "Лес",
+		imageX: 1373,
+		imageY: 225
+	},
+	{
+		text: "Дом около трассы",
+		imageX: 1414,
+		imageY: 475
+	},
+	{
+		text: "Дом с большими окнами",
+		imageX: 1593,
+		imageY: 713
+	},
+]
