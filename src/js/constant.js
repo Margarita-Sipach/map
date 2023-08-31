a490032089d2a06723854afe99c0ee0f47efc4f1
Margarita <Margarita>
2023-08-31 17:14:12 +0300

    fix: change constants structure

--- src/js/constant.js
+++ src/js/constant.js
@@ -1,5 +1,11 @@
-export const  IMAGE_WIDTH = 1920,
-              IMAGE_HEIGHT = 1080,
-							IMAGE_CENTER_X = IMAGE_WIDTH / 2,
-							IMAGE_CENTER_Y = IMAGE_HEIGHT / 2,
-							BUTTON_HEIGHT = 41;
+export const BUTTON_HEIGHT = 41;
+
+export const IMAGE_SIZE = {
+	width: 1920,
+	height: 1080,
+}
+
+export const IMAGE_CENTER = {
+	x: IMAGE_SIZE.width / 2,
+	y: IMAGE_SIZE.height / 2,
+}
