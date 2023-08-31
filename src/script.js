30d839ce6fdadb1388f178f0535b4e2b5e03acde
Margarita <Margarita>
2023-08-31 19:16:44 +0300

    feat: add click listener

--- src/script.js
+++ src/script.js
@@ -8,6 +8,20 @@ const generateAllButtons = () => {
 	buttonsData.forEach(button => app.insertAdjacentHTML('beforeend', generateButton(button)))
 }
 
+const closeButtons = () => {
+	const buttons = document.querySelectorAll(".button");
+	buttons.forEach(item => item.classList.remove("active"));
+}
+
+const onClick = (e) => {
+	const target = e.target;
+	const button = target.closest(".button");
+	const isActive = button.classList.contains("active");
+
+	closeButtons();
+	isActive ? button.classList.remove("active") : button.classList.add("active");
+}
+
 generateAllButtons()
 window.addEventListener('resize', generateAllButtons);
-
+window.addEventListener('click', onClick);
